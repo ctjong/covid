176c9036a6c231c36008cbb022bc0a7afe220138
Christopher Tjong <ctjong@linkedin.com>
2020-03-23 23:30:30 -0700

    indent

--- index.php
+++ index.php
@@ -20,53 +20,52 @@ $nyTimesContent = getContent("https://www.nytimes.com/interactive/2020/us/corona
 </head>
 
 <body style="padding:10px">
+  <?=$nyTimesContent?>
 
-<?=$nyTimesContent?>
+  <button type="button" class="btn btn-primary" onClick="app.display('stateCases')">Number of cases in the US by states</button>
+  <button type="button" class="btn btn-warning" onClick="app.display('stateDeaths')">Number of deaths in the US by states</button>
+  <button type="button" class="btn btn-danger" onClick="app.display('countyCases')">Number of cases in the US by counties</button>
 
-<button type="button" class="btn btn-primary" onClick="app.display('stateCases')">Number of cases in the US by states</button>
-<button type="button" class="btn btn-warning" onClick="app.display('stateDeaths')">Number of deaths in the US by states</button>
-<button type="button" class="btn btn-danger" onClick="app.display('countyCases')">Number of cases in the US by counties</button>
+  <div id="search" style="margin-top:20px">
+    <input type="text" id="search-text"/>
+    <button type="button" onClick="app.applySearch()">Search</button>
+    <button type="button" onClick="app.clearSearch()">Clear</button>
+  </div>
 
-<div id="search" style="margin-top:20px">
-  <input type="text" id="search-text"/>
-  <button type="button" onClick="app.applySearch()">Search</button>
-  <button type="button" onClick="app.clearSearch()">Clear</button>
-</div>
+  <div id="stateCases" class="tab-pane">
+    <h2>Number of cases in the US by states</h2>
+    <div>Source: <a target="_blank" href="https://www.nytimes.com/interactive/2020/us/coronavirus-us-cases.html">New York Times</a></div>
+    <div class="nytimes-time"></div>
+    <div id="stateCases-total"></div>
+    <canvas id="stateCases-canvas" width="2000" height="1000"></canvas>
+  </div>
 
-<div id="stateCases" class="tab-pane">
-  <h2>Number of cases in the US by states</h2>
-  <div>Source: <a target="_blank" href="https://www.nytimes.com/interactive/2020/us/coronavirus-us-cases.html">New York Times</a></div>
-  <div class="nytimes-time"></div>
-  <div id="stateCases-total"></div>
-  <canvas id="stateCases-canvas" width="2000" height="1000"></canvas>
-</div>
+  <div id="stateDeaths" class="tab-pane" style="display:none">
+    <h2>Number of deaths in the US by states</h2>
+    <div>Source: <a target="_blank" href="https://www.nytimes.com/interactive/2020/us/coronavirus-us-cases.html">New York Times</a></div>
+    <div class="nytimes-time"></div>
+    <div id="stateDeaths-total"></div>
+    <canvas id="stateDeaths-canvas" width="2000" height="1000"></canvas>
+  </div>
 
-<div id="stateDeaths" class="tab-pane" style="display:none">
-  <h2>Number of deaths in the US by states</h2>
-  <div>Source: <a target="_blank" href="https://www.nytimes.com/interactive/2020/us/coronavirus-us-cases.html">New York Times</a></div>
-  <div class="nytimes-time"></div>
-  <div id="stateDeaths-total"></div>
-  <canvas id="stateDeaths-canvas" width="2000" height="1000"></canvas>
-</div>
+  <div id="countyCases" class="tab-pane" style="display:none">
+    <h2>Number of cases in the US by counties (only showing the top 100)</h2>
+    <div>Source: <a target="_blank" href="https://www.nytimes.com/interactive/2020/us/coronavirus-us-cases.html">New York Times</a></div>
+    <div class="nytimes-time"></div>
+    <div id="countyCases-total"></div>
+    <canvas id="countyCases-canvas" width="2000" height="2000"></canvas>
+  </div>
 
-<div id="countyCases" class="tab-pane" style="display:none">
-  <h2>Number of cases in the US by counties (only showing the top 100)</h2>
-  <div>Source: <a target="_blank" href="https://www.nytimes.com/interactive/2020/us/coronavirus-us-cases.html">New York Times</a></div>
-  <div class="nytimes-time"></div>
-  <div id="countyCases-total"></div>
-  <canvas id="countyCases-canvas" width="2000" height="2000"></canvas>
-</div>
-
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
-<script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.9.3/Chart.bundle.js"></script>
-<script src="https://cdn.jsdelivr.net/npm/chartjs-plugin-datalabels@0.7.0"></script>
-<script src="constants.js"></script>
-<script src="nyTimesParser.js"></script>
-<script src="tabRenderer.js"></script>
-<script src="app.js"></script>
-<script>
-  window.app = new App();
-  window.app.main();
-</script>
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.9.3/Chart.bundle.js"></script>
+  <script src="https://cdn.jsdelivr.net/npm/chartjs-plugin-datalabels@0.7.0"></script>
+  <script src="constants.js"></script>
+  <script src="nyTimesParser.js"></script>
+  <script src="tabRenderer.js"></script>
+  <script src="app.js"></script>
+  <script>
+    window.app = new App();
+    window.app.main();
+  </script>
 </body>
 </html>
