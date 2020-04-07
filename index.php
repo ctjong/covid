8031890fec710d06b47b43da100534047e254371
Christopher Tjong <ctjong@linkedin.com>
2020-04-06 20:11:37 -0700

    server rendered title

--- index.php
+++ index.php
@@ -1,1 +1,17 @@
-<?php echo file_get_contents("index.html") ?>
+<?php 
+$titles = array(
+  "countryCases" => "Number of cases in the world by country",
+  "countryDeaths" => "Number of deaths in the world by country",
+  "countryRecovereds" => "Number of recovered cases in the world by country",
+  "stateCases" => "Number of cases in the US by state",
+  "stateDeaths" => "Number of deaths in the US by state",
+  "countyCases" => "Number of cases in the US by county (top 100)",
+  "countyDeaths" => "Number of deaths in the US by county (top 100)",
+);
+
+$content = file_get_contents("index.html");
+$tabName = $_GET["chart"];
+$title = $titles[$tabName];
+$content = str_replace("%TITLE%", $title, $content);
+echo $content;
+?>
